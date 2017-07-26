f3529cbb6d46be5392760091edb1d845b5351a66
Demi Yu <demiyuhongjun@gmail.com>
2017-07-26 14:19:19 +0800

    node-koa

---
+++ async-test/hello.js
@@ -0,0 +1,14 @@
+// hello.js
+console.log('init hello.js...');
+
+const fs = require('mz/fs');
+
+// a simple async function:
+module.exports = async () => {
+    let expression = await fs.readFile(__dirname +'/data.txt', 'utf-8');
+    // console.log(__dirname)
+    let fn = new Function('return ' + expression);
+    let r = fn();
+    console.log(`Calculate: ${expression} = ${r}`);
+    return r;
+};
